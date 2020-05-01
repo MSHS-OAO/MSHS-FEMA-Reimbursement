a33ba69ae55b1c68cfe726db62ffb66b8c0ca719
greglenane <gregory.lenane@mssm.edu>
2020-05-01 16:28:14 -0400

    Save .rds file created on local drive rather than append them through shared drive

--- Preprocess/MSH_MSQ_Preprocess.R
+++ Preprocess/MSH_MSQ_Preprocess.R
@@ -1,47 +1,13 @@
 library(tidyverse)
-library(readxl)
-library(openxlsx)
 
 setwd(dir)
+data_MSH_MSQ <- readRDS("Reference Tables/data_MSH_MSQ.rds")
 
 #Read COA for department names
 COA <- read.csv("Reference Tables/COA.csv",header = T, stringsAsFactors = F)
 
 #Read in job code descriptions
 JCdesc <- read.csv("Reference Tables/JCdesc.csv",header = T, stringsAsFactors = F)
-
-##MSQ##
-#List files from MSQ Raw folder
-setwd(paste0(dir,"MSQ Raw/"))
-folderMSQ <- paste0(dir,"MSQ Raw/")     
-MSQ_file_list <- list.files(path=folderMSQ, pattern="*.txt")
-#Read files in MSQ Raw as csv
-MSQlist = lapply(MSQ_file_list, function(x)read.csv(x, sep = ";", header=T, stringsAsFactors = F))
-#Bind all MSQ files and assign their site = "MSQ"
-MSQ = do.call("rbind", MSQlist)
-#Assign site to all of MSQ file
-MSQ <- MSQ %>%
-  mutate(WRKD.SITE = "MSQ")
-#Remove Duplicate rows
-MSQ <- MSQ %>% distinct()
-
-##MSH##
-#List files from MSH Raw folder
-setwd(paste0(dir,"MSH Raw/"))
-folderMSH <- paste0(dir,"MSH Raw/")   
-MSH_file_list <- list.files(path=folderMSH, pattern="*.txt")                              
-#Read files in MSH Raw as csv    
-MSHlist = lapply(MSH_file_list, function(x)read.csv(x, sep = ";", header=T, stringsAsFactors = F)) 
-#Bind all MSH files and assign their site = "MSH"
-MSH = do.call("rbind", MSHlist)
-#Assign site to all of MSH file
-MSH <- MSH %>%
-  mutate(WRKD.SITE = "MSH")
-#remove duplicate rows
-MSH <- MSH %>% distinct()
-
-#Bind MSH and MSQ
-data_MSH_MSQ <- rbind(MSH,MSQ)
 
 #bring in J.C description
 data_MSH_MSQ <- left_join(data_MSH_MSQ,JCdesc) 
@@ -54,7 +20,9 @@ colnames(data_MSH_MSQ)[20:21] <- c("LOCATION","DESCRIPTION")
 data_MSH_MSQ <- data_MSH_MSQ %>%
   mutate(END.DATE = as.Date(END.DATE, format = "%m/%d/%Y"),
          PAY.CODE = as.character(PAY.CODE),
+         HOURS = gsub(",","",HOURS),
          HOURS = as.numeric(HOURS),
+         EXPENSE = gsub(",","",EXPENSE),
          EXPENSE = as.numeric(EXPENSE),
          PAY.CODE = str_trim(PAY.CODE),
          HOME.SITE = rep(NA,nrow(data_MSH_MSQ)),
